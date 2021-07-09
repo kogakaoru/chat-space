2c59c62ef12a93d9065404c98c3386cda8226a94
kogakaoru <82805859+kogakaoru@users.noreply.github.com>
2021-07-10 05:14:31 +0900

    conplete ajax in messate.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,45 @@
 $(function(){
-  // console.log("あ");
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+      `<div class="main-chat__message--list--article">
+        <div class="main-chat__message--list--article--head">
+          <div class="main-chat__message--list--article--head--name">
+            ${message.user_name}
+          </div>
+          <div class="main-chat__message--list--article--head--date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="main-chat__message--list--article--text">
+          ${message.content}
+        </div>
+      </div>
+      <img src=${message.image} >`
+      
+    return html;
+    } else {
+      var html =
+      `<div class="main-chat__message--list--article">
+        <div class="main-chat__message--list--article--head">
+          <div class="main-chat__message--list--article--head--name">
+            ${message.user_name}
+          </div>
+          <div class="main-chat__message--list--article--head--date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="main-chat__message--list--article--text">
+          ${message.content}
+        </div>
+      </div>`
+      
+    return html;
+    };
+  }
+  
   $('#new_message').on('submit', function(e){
     e.preventDefault()
-    console.log("い");
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
@@ -12,6 +49,19 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      // console.log(data)
+      $('.main-chat__message--list').append(html);
+      $('.main-chat__message--list').animate({ scrollTop: $('.main-chat__message--list')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.main-chat__message--form--input--text').val('');
+      $('.main-chat__message--form--send-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+      $('.main-chat__message--form--send-btn').prop('disabled', false);
     })
   })
 });
