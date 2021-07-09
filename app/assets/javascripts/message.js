80f1bec3c33777398f90a501dc30bf3ce4da1982
kogakaoru <82805859+kogakaoru@users.noreply.github.com>
2021-07-10 03:51:03 +0900

    write about ajax in message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,17 @@
 $(function(){
-
+  // console.log("あ");
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    console.log("い");
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
