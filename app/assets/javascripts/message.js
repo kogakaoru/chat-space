62c591d864088d619d29a088ce7effa5c94c6b1a
kogakaoru <82805859+kogakaoru@users.noreply.github.com>
2021-07-17 03:54:34 +0900

    fix in message.js ("の位置・・・！)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-      `<div class="main-chat__message--list--article">
+      `<div class="main-chat__message--list--article" data-message-id=${message.id}>
         <div class="main-chat__message--list--article--head">
           <div class="main-chat__message--list--article--head--name">
             ${message.user_name}
@@ -20,7 +20,7 @@ $(function(){
     return html;
     } else {
       var html =
-      `<div class="main-chat__message--list--article">
+      `<div class="main-chat__message--list--article" data-message-id=${message.id}>
         <div class="main-chat__message--list--article--head">
           <div class="main-chat__message--list--article--head--name">
             ${message.user_name}
@@ -103,7 +103,7 @@ $(function(){
 
   function reloadMessages() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    var last_message_id = $('.main-chat__message--list--article').data("message-id");
+    var last_message_id = $('.main-chat__message--list--article:last').data("message-id");
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
@@ -113,12 +113,44 @@ $(function(){
       //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
+    // console.log(last_message_id)
     .done(function(messages) {
-      console.log('success');
+      // console.log(messages);
+      console.log("success");
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          // insertHTML += buildHTML(message)
+          insertHTML = insertHTML + buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.main-chat__message--list').append(insertHTML);
+        // $('.main-chat__message--list--article').animate({ scrollTop: $('.main-chat__message--list')[0].scrollHeight});
+        // $('.main-chat__message--form--input--text').val('');
+        $('.main-chat__message--list').animate({ scrollTop: $('.main-chat__message--list')[0].scrollHeight});
+      }
     })
+    // .done(function(messages) {
+    //   if (messages.length !== 0) {
+    //     //追加するHTMLの入れ物を作る
+    //     var insertHTML = '';
+    //     //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+    //     $.each(messages, function(i, message) {
+    //       insertHTML += buildHTML(message)
+    //     });
+    //     //メッセージが入ったHTMLに、入れ物ごと追加
+    //     $('.main-chat__message--list').append(insertHTML);
+    //     $('.main-chat__message--list').animate({ scrollTop: $('.main-chat__message--list')[0].scrollHeight});
+    //   }
+    // })
     .fail(function() {
       alert('error');
     })
-  }
+  };
+
+  // 以下は、7秒ごとにreloadMessagesを呼び出しています
+  setInterval(reloadMessages, 7000);
   
 });
